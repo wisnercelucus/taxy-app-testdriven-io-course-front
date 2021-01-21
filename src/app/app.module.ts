6611f204478a3a2bdd0f8810e6431767b3af08ac
wisnercelucus <wisnercelicus@gmail.com>
2021-01-21 17:00:56 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { LandingComponent } from './components/landing/landing.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignUpComponent,
+    LogInComponent,
+    LandingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
